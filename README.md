36d57ce440665581e8fd2a269464e688950a3b2a
Juan Alfaro <bryamalfaro@gmail.com>
2023-11-27 17:50:42 -0500

    create: first commit with the complete project

---
+++ README.md
@@ -0,0 +1,38 @@
+<div align="center" class="tip" markdown="1" style>
+![GitHub stars](https://img.shields.io/github/stars/Sheldonimo/fresh-rofi-theme) ![rep size](https://img.shields.io/github/repo-size/Sheldonimo/fresh-rofi-theme)
+</div>
+
+# Fresh Theme for Rofi Launcher
+Enhance your Rofi experience with the Fresh theme! This bash script makes it easy to install a sleek, modern theme for the Rofi launcher.
+
+### :rocket: Fresh theme
+<div align="center" class="tip" markdown="1" style>
+![Fresh theme](images/fresh-theme.webp)
+</div>
+
+## :computer: Installation
+
+To install the Fresh theme, simply clone the repository and run the installation script:
+```bash
+git clone https://github.com/Sheldonimo/fresh-rofi-theme.git
+cd fresh-rofi-theme
+chmod +x install.sh
+./install.sh
+```
+
+## Optional: Don't Have Rofi Yet? Install It!
+
+If you prefer to manually install Rofi, use the following commands depending on your distribution:
+
+- On Debian / Ubuntu: `apt-get install rofi`
+- On Arch / Arch-based: `pacman -S rofi`
+- On Fedora: `dnf install rofi`
+
+**Note:** These manual steps are optional if you're using our installation script. The script automatically checks if Rofi is installed and, if not, proceeds with the appropriate installation for your system.
+
+> Want to learn more about setting up Rofi, including keyboard shortcuts? Find additional information [here](https://github.com/davatorium/rofi).
+
+
+## What is Rofi?
+
+Rofi is a versatile and efficient tool that functions as a window switcher, application launcher, and a replacement for dmenu. Initially developed as a clone of simpleswitcher, Rofi has evolved to include additional features like an application launcher and ssh-launcher. It offers a user-friendly textual list interface for various actions, such as running applications, selecting windows, or executing options from external scripts. Rofi's functionality makes it a valuable tool for streamlining user interactions on a computer. More information about Rofi and its capabilities can be found on its [GitHub page](https://github.com/davatorium/rofi).
---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+# Definiendo variables
+root=/home/$USER
+
+# Descargando thema nord para rofi
+
+if [ ! -d "./theme" ]; then
+    echo "Please download the repository first"
+    exit 1
+fi
+
+# Instalando rofi
+
+# Comentario: El script inicia con la intención de instalar Rofi.
+
+# Verificar si Rofi está instalado
+if command -v rofi >/dev/null 2>&1; then
+    # Si Rofi ya está instalado, informa al usuario y no realiza ninguna acción adicional.
+    echo "Rofi ya está instalado."
+else
+    # Si Rofi no está instalado, inicia el proceso de instalación.
+    echo "Instalando Rofi..."
+
+    # Verifica qué administrador de paquetes está disponible para instalar Rofi.
+    # Verifica la presencia de apt-get (típico en Debian/Ubuntu).
+    if command -v apt-get >/dev/null 2>&1; then
+        echo "Actualizando paquetes con apt-get..."
+        sudo apt-get update  # Actualiza la lista de paquetes usando apt-get.
+        echo "Instalando Rofi con apt-get..."
+        sudo apt-get install -y rofi  # Instala Rofi utilizando apt-get.
+
+    # Luego verifica la presencia de pacman (típico en Arch Linux y derivados).
+    elif command -v pacman >/dev/null 2>&1; then
+        echo "Actualizando paquetes con pacman..."
+        sudo pacman -Sy  # Sincroniza y actualiza la lista de paquetes usando pacman.
+        echo "Instalando Rofi con pacman..."
+        sudo pacman -S rofi  # Instala Rofi utilizando pacman.
+
+    # Finalmente, verifica la presencia de dnf (típico en Fedora).
+    elif command -v dnf >/dev/null 2>&1; then
+        echo "Actualizando paquetes con dnf..."
+        sudo dnf check-update  # Comprueba si hay actualizaciones disponibles con dnf.
+        echo "Instalando Rofi con dnf..."
+        sudo dnf install rofi  # Instala Rofi utilizando dnf.
+
+    else
+        # Si no se encuentra ningún administrador de paquetes conocido, informa al usuario y termina el script.
+        echo "No se encontró un administrador de paquetes compatible."
+        exit 1
+    fi
+fi
+
+## Moviendo el nuevo rofi theme
+
+echo "Moviendo el rofi theme a $root/.config/rofi/themes"
+
+[ ! -d "$root/.config/rofi/themes" ] && mkdir -p "$root/.config/rofi/themes"
+if [ ! -f $root/.config/rofi/themes/nord-fresh.rasi ]; then
+  cp ./theme/nord-fresh.rasi $root/.config/rofi/themes/nord-fresh.rasi
+  sudo mv ./theme/nord-fresh.rasi /usr/share/rofi/themes/nord-fresh.rasi
+  cp ./theme/config.rasi $root/.config/rofi/config.rasi
+fi
